863740c8acef25f7eeac2aa457a42f5cfefc1381
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-08 20:21:21 +0100

    updating ..

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView.builder(
+          itemCount: 5,
+          itemBuilder: (context, position){
+        return _buildPageItem(position);
+      }),
+    );
+  }
+  Widget _buildPageItem(int index){
+    return Container(
+
+    );
+  }
+}
