d62e6e3ff522e7a0a9b33a0f7ef57b5a9aaec511
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-08 18:56:02 +0100

    updating ..

--- lib/widgets/small_text.dart
+++ lib/widgets/small_text.dart
@@ -5,7 +5,7 @@ class SmallText extends StatelessWidget {
   final String text;
   double size;
   TextOverflow overFlow;
-  BigText({Key? key, this.color = const Color(0xFF332d2b),
+  SmallText({Key? key, this.color = const Color(0xFF332d2b),
     required this.text,
     this.size=20,
     this.overFlow=TextOverflow.ellipsis
