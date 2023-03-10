98259c7ee3a061dbc8e10f3dcbae1f97e44ef213
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-10 18:18:56 +0100

    updating ..

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -12,6 +12,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
           itemCount: 5,
@@ -26,7 +27,13 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       margin: EdgeInsets.only(left: 5, right: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
-        color: Color(0xFF69c5df)
+        color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            "assets/image/food0.png"
+      )
+      )
       ),
     );
   }
