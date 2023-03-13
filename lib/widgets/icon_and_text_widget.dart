01d8baeee0785cf25e945c4ddf5949e4cafea4e6
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-13 17:50:50 +0100

    updating ..

--- lib/widgets/icon_and_text_widget.dart
+++ lib/widgets/icon_and_text_widget.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
 class IconAndTextWidget extends StatelessWidget {
-  const IconAndTextWidget({Key? key}) : super(key: key);
+  final IconData icon;
+  final String text;
+  final Color color;
+  final Color IconColor;
+  const IconAndTextWidget({Key? key,
+    required this.icon,
+    required this.text,
+    required this.color,
+    required this.IconColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
