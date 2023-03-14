19ff9b1f2beee489b6b7de1e8c3092cc43ce71a2
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-14 16:31:02 +0100

    updating ..

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -99,9 +99,16 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                       color: Color(0xFFe8e8e8),
                       blurRadius: 5.0,
                       offset: Offset(0, 5)
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5, 0)
+                    ),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(5, 0)
                     )
                   ]
-
               ),
               child: Container(
                 padding: EdgeInsets.only(top: 10, left: 15, right: 15),
