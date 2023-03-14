5d949a1290c6ce6bff9132db865c9c665b81a21a
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-14 11:49:32 +0100

    updating ..

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -52,77 +52,81 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
     }else if(index ==_currPageValue.floor()+1){
       var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
     }
 
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(
-                      "assets/image/food0.png"
-                  )
-              )
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chinese Side"),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) {return Icon(Icons.star,color: AppColors.mainColor, size: 15,);}),
-                      ),
-                      SizedBox(width: 10,),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "1287"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "comments"),
-                    ],
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    children: [
-                      IconAndTextWidget(icon: Icons.circle_sharp,
-                          text: "Normal",
-
-                          iconColor: AppColors.iconColor1),
-                      IconAndTextWidget(icon: Icons.location_on,
-                          text: "1.7km",
-
-                          iconColor: AppColors.mainColor),
-                      IconAndTextWidget(icon: Icons.access_time_rounded,
-                          text: "32min",
-
-                          iconColor: AppColors.iconColor2)
-                    ],
-                  )
-                ],
-              ),
+                color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                        "assets/image/food0.png"
+                    )
+                )
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {return Icon(Icons.star,color: AppColors.mainColor, size: 15,);}),
+                        ),
+                        SizedBox(width: 10,),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp,
+                            text: "Normal",
+
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(icon: Icons.location_on,
+                            text: "1.7km",
+
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(icon: Icons.access_time_rounded,
+                            text: "32min",
+
+                            iconColor: AppColors.iconColor2)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
