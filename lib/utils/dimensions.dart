37834c6f2fc023113a68eeab7f8955cdc6d9ef9c
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-15 15:22:51 +0100

    updating ..

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -3,7 +3,8 @@ class Dimensions {
 
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
-  static double pageView= screenHeight/3.84;
+
+  static double pageView= screenHeight/2.64;
   static double pageViewContainer= screenHeight/3.84;
   static double pageViewTextContainer= screenHeight/7.03;
 
