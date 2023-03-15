a04668330903d59ffe36ba9dd1117d1cf552d68f
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-15 16:06:47 +0100

    updating ..

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -12,6 +12,10 @@ class Dimensions {
   static double height15 = screenHeight/56.27;
   static double height20 = screenHeight/42.2;
   //dynamic width padding and margin
+  static double width10 = screenHeight/84.4;
+  static double width15 = screenHeight/56.27;
+  static double width20 = screenHeight/42.2;
+
   static double font20 = screenHeight/42.2;
   static double radius20 = screenHeight/42.2;
   static double radius30 = screenHeight/28.13;
