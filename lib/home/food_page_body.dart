8ed2d2271c0494cbda75646762e1d1b097df96c5
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-15 18:19:56 +0100

    updating ..

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -84,6 +84,33 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           ),
         ),
         //list of food and images
+        Container(
+          height: 900,
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index){
+                return Container(
+                  margin: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(Dimensions.radius20),
+                            color: Colors.white38,
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "assets/image/food0.png"
+                                )
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }),
+        )
       ],
     );
   }
