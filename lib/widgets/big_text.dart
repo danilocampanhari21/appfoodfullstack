bd81cf736650e410a2577596a8e1f9538d5d428a
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-15 15:58:35 +0100

    updating ..

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,3 +1,4 @@
+import 'package:appfoodfullstack/utils/dimensions.dart';
 import 'package:flutter/cupertino.dart';
 
 class BigText extends StatelessWidget {
@@ -20,7 +21,7 @@ class BigText extends StatelessWidget {
       style: TextStyle(
         fontFamily: 'Roboto',
         color: color,
-        fontSize: size,
+        fontSize: Dimensions.font20,
         fontWeight: FontWeight.w400
       ),
     );
