5500ad0b0380141f4fa4d5417f98b93a2fbb6eaa
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 18:43:55 +0100

    updating ..

--- lib/widgets/exandable_text_widgets.dart
+++ lib/widgets/exandable_text_widgets.dart
@@ -1,5 +1,9 @@
 import 'package:appfoodfullstack/utils/dimensions.dart';
+import 'package:appfoodfullstack/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
 
 class ExpandableTextWidget extends StatefulWidget {
   final String text;
@@ -23,11 +27,31 @@ class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
     if(widget.text.length>textHeight){
       firstHalf = widget.text.substring(0, textHeight.toInt());
       secondHalf = widget.text.substring(textHeight.toInt()+1, widget.text.length);
+    }else{
+      firstHalf=widget.text;
+      secondHalf="";
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: secondHalf.isEmpty?SmallText(text: firstHalf):Column(
+        children: [
+          SmallText(text:hiddenText?(firstHalf+"..."):(firstHalf+secondHalf)),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Row(
+              children: [
+                SmallText(text: "Show more", color: AppColors.mainColor,),
+                Icon(Icons.arrow_drop_down, color: AppColors.mainColor,),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
