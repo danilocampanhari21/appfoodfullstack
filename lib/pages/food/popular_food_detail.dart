f8d857b46cb7ee8f5003119588157cb8cbb187fb
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 18:18:56 +0100

    updating ..

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -71,7 +71,8 @@ class PopularFoodDetail extends StatelessWidget {
                   ],
                 ),
 
-          ))
+          )),
+          //expandable the widget
         ],
       ),
       bottomNavigationBar: Container(
