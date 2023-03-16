4696feaf06a8d8c944da1862da90fbddb2acedca
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 17:50:16 +0100

    updating ..

--- lib/widgets/app_column.dart
+++ lib/widgets/app_column.dart
@@ -16,7 +16,7 @@ class AppColumn extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        BigText(text: text),
+        BigText(text: text, size: Dimensions.font26,),
         SizedBox(height: Dimensions.height10,),
         //comments section
         Row(
