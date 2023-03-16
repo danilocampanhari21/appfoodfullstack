e7982434b3cb8a0d8a1f140c3348c5a9c2286137
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 16:05:05 +0100

    updating ..

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              right: 0,
+              left: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/image/food.png"
+                    )
+                  )
+                ),
+          ))
+        ],
+      ),
+    );
   }
 }
