f1a785b5e04e4cce592fa4210c9697910e0314f4
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 16:29:26 +0100

    updating ..

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -14,6 +14,18 @@ class AppIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(size/2),
+        color: backgroundColor
+      ),
+      child: Icon(
+        icon,
+        color: iconColor,
+        size: 16,
+      ),
+    );
   }
 }
