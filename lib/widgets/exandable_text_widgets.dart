2229f8a983ddcc89456c0747459ffaacab047ac1
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-16 18:30:34 +0100

    updating ..

--- lib/widgets/exandable_text_widgets.dart
+++ lib/widgets/exandable_text_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:appfoodfullstack/utils/dimensions.dart';
 import 'package:flutter/cupertino.dart';
 
 class ExpandableTextWidget extends StatefulWidget {
@@ -13,6 +14,17 @@ class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
   late String secondHalf;
 
   bool hiddenText=true;
+
+  double textHeight = Dimensions.screenHeight/5.63;
+
+  @override
+  void initState(){
+    super.initState();
+    if(widget.text.length>textHeight){
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf = widget.text.substring(textHeight.toInt()+1, widget.text.length);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
