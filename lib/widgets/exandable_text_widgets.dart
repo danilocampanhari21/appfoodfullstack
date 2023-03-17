c74a2f9fa85c9ef49f0d77742bcc4262e33b01a3
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-17 17:05:10 +0100

    updating ..

--- lib/widgets/exandable_text_widgets.dart
+++ lib/widgets/exandable_text_widgets.dart
@@ -38,7 +38,7 @@ class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
     return Container(
       child: secondHalf.isEmpty?SmallText(size:Dimensions.font16,text: firstHalf):Column(
         children: [
-          SmallText(size:Dimensions.font16,text:hiddenText?(firstHalf+"..."):(firstHalf+secondHalf)),
+          SmallText(color:AppColors.textColor,size:Dimensions.font16,text:hiddenText?(firstHalf+"..."):(firstHalf+secondHalf)),
           InkWell(
             onTap: (){
               setState(() {
