8e660ad9d1abea8d0198ab8d4ede5a4d1d88adf6
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-20 20:07:41 +0100

    updating ..

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:appfoodfullstack/pages/food/recommended_food_detail.dart';
 import 'package:appfoodfullstack/pages/home/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-void main() {
+import 'helper/dependencies.dart' as dep;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
