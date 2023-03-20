24bceb04ac07fac2f58c9750241178342c71f025
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-20 19:40:18 +0100

    updating ..

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import '../data/repository/popular_product_repo.dart';
+
+class PopularProductController extends GetxController{
+  final PopularProductRepo popularProductRepo;
+}
