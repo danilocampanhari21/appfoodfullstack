9913f182675f6ee4726845f09ea0778683817814
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-20 19:18:44 +0100

    updating ..

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -4,6 +4,7 @@ class ApiClient extends GetConnect implements GetxService{
   late String token;
   final String appBaseUrl;
   ApiClient({ required this.appBaseUrl}){
-
+    baseUrl = appBaseUrl;
+    timeout = Duration(seconds: 30);
   }
 }
