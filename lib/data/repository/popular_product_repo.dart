04d5d689d5dedb5489241a1be18d3b3e5ec9c4be
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-20 19:34:27 +0100

    updating ..

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -6,6 +6,6 @@ class PopularProductRepo extends GetxService{
   PopularProductRepo({required this.apiClient})
 
   Future<Response> getPopularProductList() async{
-
+    return await apiClient.getData("end point url");
   }
 }
