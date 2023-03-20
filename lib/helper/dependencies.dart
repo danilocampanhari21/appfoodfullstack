c37f30aebcaf2805ce5981e669d438c5ba76a1c5
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-20 20:00:36 +0100

    updating ..

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,11 @@
+import 'package:get/get.dart';
+
+import '../data/api/api_client.dart';
+import '../data/repository/popular_product_repo.dart';
+
 Future<void> init()async {
 
+  Get.lazyPut(()=>ApiClient(appBaseUrl: "https://www.dbestech"));
+
+  Get.lazyPut(()=>PopularProductRepo(apiClient:Get.find()));
 }
