51ea00ba5ea152864c8e918e25339869e57f8919
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-22 20:56:15 +0100

    updating ..

--- lib/models/products_model.dart
+++ lib/models/products_model.dart
@@ -3,4 +3,18 @@ class Product {
   int? typeId;
   int? offset;
   List<Products>? products;
+
+  Product({this.totalSize, this.typeId, this.offset, this.products});
+
+  Product.fromJson(Map<String, dynamic> json) {
+    totalSize = json['total_size'];
+    typeId = json['type_id'];
+    offset = json['offset'];
+    if(json['products'] != null) {
+      products = <Products>[];
+      json['products'].forEach((v) {
+        products!.add(new Products.fromJson(v));
+      })
+    }
+  }
 }
