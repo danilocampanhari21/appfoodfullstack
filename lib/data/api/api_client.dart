098c3010a18f3ffd309f49fd08bf67372302da88
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-23 22:53:39 +0100

    updating ..

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -3,14 +3,16 @@ import 'package:get/get.dart';
 class ApiClient extends GetConnect implements GetxService{
   late String token;
   final String appBaseUrl;
+
   late Map<String, String> _mainHeaders;
 
   ApiClient({ required this.appBaseUrl}){
     baseUrl = appBaseUrl;
     timeout = Duration(seconds: 30);
+    token ="";
     _mainHeaders={
       'Content-type':'application/json; charset=UTF-8',
-      'Authorization': 'Bear $token'
+      'Authorization': 'Bear $token',
     };
   }
   Future<Response> getData(String uri,) async {
