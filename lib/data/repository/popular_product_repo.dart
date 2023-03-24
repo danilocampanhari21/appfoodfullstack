86f29b27fe86b3dedf737c4335832a1f325d38b0
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-24 18:06:04 +0100

    updating ..

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -1,11 +1,13 @@
 import 'package:appfoodfullstack/data/api/api_client.dart';
 import 'package:get/get.dart';
 
+import '../../utils/app_contants.dart';
+
 class PopularProductRepo extends GetxService{
   final ApiClient apiClient;
   PopularProductRepo({required this.apiClient});
 
   Future<Response> getPopularProductList() async{
-    return await apiClient.getData("/api/v1/products/popular");
+    return await apiClient.getData(AppConstants.BASE_URL);
   }
 }
