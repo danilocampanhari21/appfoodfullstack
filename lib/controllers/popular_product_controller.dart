432ae221f442674660eed5ab41225594cc6729c8
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-24 19:43:37 +0100

    updating ..

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -12,7 +12,7 @@ class PopularProductController extends GetxController{
   Future<void> getPopularProductList()async{
     Response response = await popularProductRepo.getPopularProductList();
     if(response.statusCode==200){
-      print("got Products");
+      print("got products");
       _popularProductList=[];
       _popularProductList.addAll(Product.fromJson(response.body).products);
       //print(_popularProductList);
