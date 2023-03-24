545ef25e7ca2ae8f7a3cdf3c8055de403637b05c
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-24 19:37:30 +0100

    updating ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:appfoodfullstack/controllers/popular_product_controller.dart';
 import 'package:appfoodfullstack/pages/food/popular_food_detail.dart';
 import 'package:appfoodfullstack/pages/food/recommended_food_detail.dart';
+import 'package:appfoodfullstack/pages/home/food_page_body.dart';
 import 'package:appfoodfullstack/pages/home/main_food_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: RecommendedFoodDetail(),
+      home: FoodPageBody(),
     );
   }
 }
