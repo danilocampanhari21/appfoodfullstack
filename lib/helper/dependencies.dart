b1fd57246e2342fedb2409f9af05ed74a3c63df4
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-24 18:07:21 +0100

    updating ..

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -3,10 +3,11 @@ import 'package:get/get.dart';
 import '../controllers/popular_product_controller.dart';
 import '../data/api/api_client.dart';
 import '../data/repository/popular_product_repo.dart';
+import '../utils/app_contants.dart';
 
 Future<void> init()async {
   //api client
-  Get.lazyPut(()=>ApiClient(appBaseUrl: "https://mvs.bslmeiyu.com"));
+  Get.lazyPut(()=>ApiClient(appBaseUrl:AppConstants.BASE_URL));
 
   //repos
   Get.lazyPut(()=>PopularProductRepo(apiClient:Get.find()));
