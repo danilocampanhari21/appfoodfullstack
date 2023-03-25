4ffe139773cbe976e213347984b68b6aaf1f4675
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-25 17:20:33 +0100

    updating ..

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -4,7 +4,6 @@ import '../controllers/popular_product_controller.dart';
 import '../data/api/api_client.dart';
 import '../data/repository/popular_product_repo.dart';
 import '../utils/app_contants.dart';
-
 Future<void> init()async {
   //api client
   Get.lazyPut(()=>ApiClient(appBaseUrl:AppConstants.BASE_URL));
