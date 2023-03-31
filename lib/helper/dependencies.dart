42c2626b4cb7870b359a13bf340294d6aaed713d
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-31 18:43:55 +0200

    updating ..

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,16 +1,22 @@
-import 'package:get/get.dart';
 
 import '../controllers/popular_product_controller.dart';
+import '../controllers/recommended_product_controller.dart';
 import '../data/api/api_client.dart';
 import '../data/repository/popular_product_repo.dart';
+import '../data/repository/recommended_product_repo.dart';
 import '../utils/app_constants.dart';
+
+import 'package:get/get.dart';
+
 Future<void> init()async {
   //api client
   Get.lazyPut(()=>ApiClient(appBaseUrl:AppConstants.BASE_URL));
 
   //repos
   Get.lazyPut(()=>PopularProductRepo(apiClient:Get.find()));
+  Get.lazyPut(()=>RecommendedProductRepo(apiClient:Get.find()));
 
   //controllers
   Get.lazyPut(()=>PopularProductController(popularProductRepo:Get.find()));
+  Get.lazyPut(()=>RecommendedProductController(recommendedProductRepo:Get.find()));
 }
