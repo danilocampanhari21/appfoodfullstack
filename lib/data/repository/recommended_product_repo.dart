2ceae4375ca6a69b4587bce73640615606015527
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-03-31 17:05:36 +0200

    updating ..

---
+++ lib/data/repository/recommended_product_repo.dart
@@ -0,0 +1,13 @@
+import 'package:appfoodfullstack/data/api/api_client.dart';
+import 'package:get/get.dart';
+
+import '../../utils/app_contants.dart';
+
+class RecommendedProductRepo extends GetxService{
+  final ApiClient apiClient;
+  RecommendedProductRepo({required this.apiClient});
+
+  Future<Response> getPopularRecommendedList() async{
+    return await apiClient.getData(AppConstants.RECOMMENDED_PRODUCT_URI);
+  }
+}
