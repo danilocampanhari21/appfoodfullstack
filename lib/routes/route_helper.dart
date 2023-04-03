6128e79b6d8dc61e2495d5d2d9e7298cd7ffd922
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-03 15:21:36 +0200

    updating ..

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -4,6 +4,7 @@ import '../pages/home/main_food_page.dart';
 
 class RouteHelper {
   static const String initial="/";
+  static const String popularFood="/popular-food";
 
 
   static List<GetPage> routes=[
