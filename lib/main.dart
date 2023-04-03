931a8bc5810278248dc963968509257c6163b2dc
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-03 15:11:41 +0200

    updating ..

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
 
-        primarySwatch: Colors.blue,
-      ),
       home: MainFoodPage(),
     );
   }
