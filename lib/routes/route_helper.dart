a836e2e7c2cbef8d695c6ccfce884b7927cc1aea
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-03 16:50:59 +0200

    updating ..

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -7,12 +7,12 @@ class RouteHelper {
   static const String initial="/";
   static const String popularFood="/popular-food";
 
-  static String GetInitial()=>'/';
+  static String GetInitial()=>'$initial';
   static String getPopularFood()=>'$popularFood';
 
 
   static List<GetPage> routes=[
-    GetPage(name: "/", page: ()=>MainFoodPage()),
+    GetPage(name: initial, page: ()=>MainFoodPage()),
 
     GetPage(name:"/popular-food", page:(){
       //print("popular food pegou o chamado");
