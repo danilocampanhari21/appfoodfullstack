ce19e9159e549fa54c05d5126e8a6b011873ec92
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-10 11:47:18 +0200

    updating ..

--- lib/pages/home/food_page_body.dart
+++ lib/pages/home/food_page_body.dart
@@ -108,7 +108,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
         GetBuilder<RecommendedProductController>(builder: (recommendedProduct){
           return recommendedProduct.isLoaded?ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
+              physics: NeverScrollableScrollPhysics(),
               shrinkWrap: true,
               itemCount: recommendedProduct.recommendedProductList.length,
               itemBuilder: (context, index){
