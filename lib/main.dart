59feff9d3d5f0266be8d5dac77cb8852fb8abdde
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-04-10 17:47:16 +0200

    updating ..

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
 
-      home: const RecommendedFoodDetail(),
+      home: RecommendedFoodDetail(),
       initialRoute: RouteHelper.initial,
       getPages: RouteHelper.routes,
     );
